776468e813e92ade8210ed4d0cd27c1b1c6702d3
Wayne Bloss <waynebloss@gmail.com>
2020-03-27 17:39:36 -0400

    Implement time-saving auto-relogin for API developer interface.

--- Backend/jwcs-vendor-api/src/app.js
+++ Backend/jwcs-vendor-api/src/app.js
@@ -169,6 +169,7 @@ function useSwaggerJson() {
 }
 
 function useSwaggerUI() {
+  // Serve the Swagger UI.
   // See:
   // - https://www.npmjs.com/package/swagger-ui-express#custom-swagger-options
   // - https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/configuration.md#display
@@ -178,46 +179,100 @@ function useSwaggerUI() {
     swaggerUI.setup(apiSpec, {
       swaggerOptions: {
         docExpansion: "none",
-        // plugins: [
-        //   {
-        //     afterLoad: function(ui) {
-        //       // See https://github.com/swagger-api/swagger-ui/blob/master/src/core/system.js#L344
-        //       // See https://github.com/swagger-api/swagger-ui/issues/2915#issuecomment-297405865
-
-        //       // var swaggerOptions = this;
-
-        //       // TODO: Get auth token from localStorage, if any.
-
-        //       // TODO: Put auth token into Swagger:
-        //       // ui.authActions.authorize({ AuthJWT: { name: "AuthJWT", schema: {type:"apiKey", in: "header", name: "Authorization", description: ""}, value: "<THE TOKEN>"}});
-
-        //       // TODO: On interval, Call ui.getState(); and get auth token from:
-        //       // "auth.authorized[SCHEMA_NAME].value", by finding strings in _root.entries[]...
-        //       // _root.entries[4][1]._root.entries[0][1]._root.entries[0][1]._root.entries[2][1]
-
-        //       // TODO: On interval, save fresh auth token into localStorage.
-
-        //       // TEST: To see if this will work... (it will!)
-        //       setTimeout(function() {
-        //         ui.authActions.authorize({
-        //           AuthJWT: {
-        //             name: "AuthJWT",
-        //             schema: {
-        //               type: "apiKey",
-        //               in: "header",
-        //               name: "Authorization",
-        //               description: "",
-        //             },
-        //             value: "<YOUR JWT TOKEN HERE>",
-        //           },
-        //         });
-        //       }, 1000);
-        //     },
-        //   },
-        // ],
+        plugins: [useSwaggerUIAuthStoragePlugin()],
       },
     }),
   );
+}
+
+function useSwaggerUIAuthStoragePlugin() {
+  /* eslint-disable */
+  // prettier-ignore
+  const afterLoad = function(ui) {
+    // NOTE: Code inside this afterLoad function will run in the browser!
+    //
+    // **Therefore, you cannot use an closure variables in here!**
+    // Also you should follow ES5 coding style.
+    //
+    // See: https://github.com/scottie1984/swagger-ui-express/blob/master/index.js#L239
+    //
+    // Other Notes:
+    // See https://github.com/scottie1984/swagger-ui-express/issues/44
+    // See https://github.com/swagger-api/swagger-ui/blob/master/src/core/system.js#L344
+    // See https://github.com/swagger-api/swagger-ui/issues/2915#issuecomment-297405865
+
+    /** The name of one of your API securitySchemes. */
+    var AUTH_SCHEME = "AuthJWT";
+    // var swaggerOptions = this;
+    var currentAuthToken = undefined;
+
+    setTimeout(function() {
+      // Restore auth token from localStorage, if any.
+      var token = localStorage.getItem(AUTH_SCHEME);
+      if (token) {
+        setAuthToken(token);
+        console.log("Restored " + AUTH_SCHEME + " token from localStorage.");
+      }
+      // Start polling ui.getState() to see if the user changed tokens.
+      setTimeout(checkForNewLogin, 3000);
+    }, 1000);
+
+    function checkForNewLogin() {
+      var stateToken = getAuthTokenFromState();
+      if (stateToken !== currentAuthToken) {
+        console.log("Saved " + AUTH_SCHEME + " token to localStorage.");
+        if (stateToken) {
+          localStorage.setItem(AUTH_SCHEME, stateToken);
+        } else {
+          localStorage.removeItem(AUTH_SCHEME);
+        }
+        currentAuthToken = stateToken;
+      }
+      // Continue checking every second...
+      setTimeout(checkForNewLogin, 1000);
+    }
+
+    function getAuthTokenFromState() {
+      var state = ui.getState();
+      // Get token from state "auth.authorized[AUTH_SCHEME].value"
+      return getUIStateEntry(
+        getUIStateEntry(
+          getUIStateEntry(getUIStateEntry(state, "auth"), "authorized"),
+          AUTH_SCHEME
+        ),
+        "value"
+      );
+    }
+
+    function getUIStateEntry(state, name) {
+      if (state && state._root && Array.isArray(state._root.entries)) {
+        var entry = state._root.entries.find(e => e && e[0] === name);
+        return entry ? entry[1] : undefined;
+      }
+      return undefined;
+    }
+
+    function setAuthToken(token) {
+      var authorization = {};
+      authorization[AUTH_SCHEME] = {
+        name: AUTH_SCHEME,
+        schema: {
+          type: "apiKey",
+          in: "header",
+          name: "Authorization",
+          description: "",
+        },
+        value: token,
+      };
+      var result = ui.authActions.authorize(authorization);
+      currentAuthToken = token;
+      return result;
+    }
+  };
+  /* eslint-enable */
+  return {
+    afterLoad,
+  };
 }
 
 function writeDeveloperErrorHelp(err) {
